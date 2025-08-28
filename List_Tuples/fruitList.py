fcd08b5d49f2f4f715c0b3700398031f15f71b20
AkshayMohan <akshay.mohan@genworkshealth.com>
2025-08-28 13:35:10 +0530

    List & Tuple

---
+++ List_Tuples/fruitList.py
@@ -0,0 +1,16 @@
+list = []
+
+f1 = input("Enter your fruit : ")
+list.append(f1)
+f2 = input("Enter your fruit : ")
+list.append(f2)
+f3 = input("Enter your fruit : ")
+list.append(f3)
+f4 = input("Enter your fruit : ")
+list.append(f4)
+f5 = input("Enter your fruit : ")
+list.append(f5)
+f6 = input("Enter your fruit : ")
+list.append(f6)
+
+print(list)
---
+++ List_Tuples/studentMarks.py
@@ -0,0 +1,17 @@
+marks = []
+
+sm1 = int(input("mark : "))
+marks.append(sm1)
+
+sm2 = int(input("mark : "))
+marks.append(sm2)
+
+sm3 = int(input("mark : "))
+marks.append(sm3)
+
+sm4 = int(input("mark : "))
+marks.append(sm4)
+
+marks.sort()
+
+print(marks)
